ca7594a5fea475bbb121abffaf52c83c4e05acb3
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-01-07 02:20:36 -0300

    fix: Correção no algoritmo de soma

--- algorithms/sum.js
+++ algorithms/sum.js
@@ -1,32 +1,8 @@
-class Votes {
-    totalVoters = 1000;
-    votesValid = 800;
-    votesBlank = 150;
-    votesNull = 50;
-
-    formatPercent(numb) {
-        return (numb * 100) + '%';
-    }
-
-    getValidVotesPercent() {
-        return this.formatPercent(this.votesValid / this.totalVoters);
-    }
-
-    getBlankVotesPercent() {
-        return this.formatPercent(this.votesBlank / this.totalVoters);
-    }
-
-    getNullVotesPercent() {
-        return this.formatPercent(this.votesNull / this.totalVoters);
-    }
+function sum(x) {
+    return [...Array(x).keys()]
+        .filter(val => val % 3 == 0 || val % 5 == 0)
+        .filter(val => val > 0);
 }
 
-const votes = new Votes();
-const validVotesPercent = votes.getValidVotesPercent();
-console.log(`Percentual de votos válidos: ${validVotesPercent}.`);
-
-const blankVotesPercent = votes.getBlankVotesPercent();
-console.log(`Percentual de votos em branco: ${blankVotesPercent}.`);
-
-const nullVotesPercent = votes.getNullVotesPercent();
-console.log(`Percentual de votos nulo: ${nullVotesPercent}.`);
+const sumOfMultiples = sum(10);
+console.log(sumOfMultiples);
